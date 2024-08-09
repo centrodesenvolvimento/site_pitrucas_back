8482dafd86830568d5f5f00d5ca79b8fe709ec2e
Ricardo-Cambundo <ricardocmbd@gmail.com>
2024-08-09 16:40:27 +0100

    something

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
             AboutContentSeeder::class,
 
             HomeContentSeeder::class,
-            AdmissionsContent::class,
+            AdmissionsContentSeeder::class,
             DepartamentosSeeder::class,
             InfoSeeder::class,
             NewsSeeder::class
